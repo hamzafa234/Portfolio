a3b7ce01ff8c8c588eaa1cfe5aff632b4c78e9c5
hamzafa234 <hamzafa234@gmail.com>
2025-12-15 10:00:57 -0600

    fixed spelling error

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return(
     <>
       <div className="grid-container">
-        <Bigbox title="Hamza Fahad" paragraph="I am a college sophomore and have been coding since highschool. I have strong soft skills such as communication and knowing how to work in a team."/>
+        <Bigbox title="Hamza Fahad" paragraph="I am a college sophomore and have been coding since high school. I have soft skills such as communication and knowing how to work in a team."/>
         <Bigbox 
           title="Technologies" 
           paragraph={<><Smallbox word="Java" />  <Smallbox word="Python" /> <Smallbox word="ZSH" /> <Smallbox word="Linux" /> <Smallbox word="React" /> <Smallbox word="Git" /> <Smallbox word="Vim" /></>} 
